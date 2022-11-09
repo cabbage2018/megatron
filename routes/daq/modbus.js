788e3e35cd9141cf85187a136dc8dc3c61557705
Gao <fei-gao@siemens.com>
2022-11-09 17:24:05 +0800

    Fix syntax bugs

--- routes/daq/modbus.js
+++ routes/daq/modbus.js
@@ -17,9 +17,9 @@ let {
 ///modbus/access?ip='10.34.59.228'&port=503&subnum=126
 router.get("/modbus/access/:ip/:port/:subnum", async (req, res, next) => {
 	let physical = {};
-	physical.ip = req.query.ip ?? '127.0.0.1';
-	physical.port = req.query.port ?? 502;
-	physical.subnum = req.query.subnum ?? 127;
+	physical.ip = req.query.ip ? req.query.ip : '127.0.0.1';
+	physical.port = req.query.port ? req.query.port : 502;
+	physical.subnum = req.query.subnum ? req.query.subnum : 127;
 	physical.responses = [];
 	await access(physical.ip, physical.port, physical.subnum)
 		.then((response) => {
@@ -35,14 +35,14 @@ router.get("/modbus/access/:ip/:port/:subnum", async (req, res, next) => {
 router.get("/modbus/acquire/:uri", async (req, res, next) => {
 	// let uri = req.params('uri') ?? '';
 	let x = {};
-	x.ip = req.params('ip') ?? '127.0.0.1';
-	x.port = req.params('port') ?? 502;
-	x.subnum = req.params('subnum') ?? 127;
-	x.fc = req.params('fc') ?? 3;
-	x.reg = req.params('reg') ?? 3021;
-	x.quantity = req.params('quantity') ?? 127;
-	x.timeout = req.params('timeout') ?? 6000;
-	x.flash = req.params('flash') ?? [255, 0, 129];
+	x.ip = req.params('ip') ? req.params('ip') : '127.0.0.1';
+	x.port = req.params('port') ? req.params('port') : 502;
+	x.subnum = req.params('subnum') ? req.params('subnum') : 127;
+	x.fc = req.params('fc') ? req.params('fc') : 3;
+	x.reg = req.params('reg') ? req.params('reg') : 3021;
+	x.quantity = req.params('quantity') ? req.params('quantity') : 127;
+	x.timeout = req.params('timeout') ? req.params('timeout') : 6000;
+	x.flash = req.params('flash') ? req.params('flash') : [255, 0, 129];
 	x.responses = [];
 	await acquire(x.ip, x.port, x.subnum, x.fc, x.reg, x.quantity, x.timeout, x.flash/**/)
 		.then((response) => {
