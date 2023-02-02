edb86cf8d0ef4818d8187a89bf459b74929b951b
cabb2018 <gaof2001@hotmail.com>
2023-02-02 08:06:49 +0000

    Insert a diagram updating live data thru jQuery

--- routes/chart/index.js
+++ routes/chart/index.js
@@ -17,4 +17,18 @@ router.get('/ts', function (req, res, next) {
     })
     // log.debug(req, res, next);
 })
+
+router.get('/updates', function (req, res, next) {
+    let arr = [95.6, 54.4, 29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 33.2];
+    let str = JSON.stringify(arr);
+    res.write(' ' + str + ' ');
+    res.end();
+})
+
+router.get('/diagram', function (req, res, next) {
+    res.render('diagram', {
+        title: 'Data is alive.',
+    })
+})
+
 module.exports = router;
