c8465533c6d2acc95f5794577a5dada5100ab240
Fei <gaof2001@hotmail.com>
2023-05-19 16:53:53 +0800

    Rewrite modbus tcp as class, event emitter embedded too

--- conn/daq/modbustcp/index.js
+++ conn/daq/modbustcp/index.js
@@ -1,119 +1,173 @@
-'use strict'
+"use strict";
+const util = require('util')
+const events = require('events')
+const cache = (o) => { fs.writeFileSync(path.join(process.cwd(), this.cacheFilePath), JSON.stringify(o), 'utf-8') }
+const request = require('supertest')
+const path = require('path')
+const fs = require('fs')
 const log4js = require('log4js')
-const log = log4js.getLogger('conn::daq::modbustcp:index')
-let inventory = require('./modbus')
-let {
-	// array,
-	measure,
-	tariff,
-	schedule,
-} = require('./commissioning')
-let {
-	acquire,
-	access
-} = require('./modbus')
+const log = log4js.getLogger(':modbustcp:')
 
-module.exports = {
-	acquire: acquire,
-	access: access,
-	measure: measure,
-	tariff: tariff,
-	schedule: schedule,
-	commission: async function (req, res, next) {
-		if (res.physicals && res.physicals.length > 0) {
-		}
-		return
-	},
-	instantiate: function (req, res, next) {
-		if (req.candidates) {
-			for (let i = 0; i < req.candidates.length; i = i + 1) {
-				let addr = req.candidates[i]
-				req.serviceProvider = search(addr.model)
-				if (req.serviceProvider.space && req.serviceProvider.layout) {
-					for (let j = 0; j < req.serviceProvider.space.length; j = j + 1) {
-						let sample = req.serviceProvider.space[j]
-						let scramble = {
-							ip: addr.ip,
-							port: addr.port,
-							subordinatorNumber: addr.subordinatorNumber,
-							model: addr.model,
+class modbustcp {
+	/**
+	 * 
+	 */
+	constructor() {
+		this.des = "save ecosystem with limiting usage of resource and energy";
+		this.identity = "modbustcp plugin";
+		this.client = undefined;
+	}
 
-							// protocol: req.physical.protocol,
-							timeoutMillisecond: addr.timeoutMillisecond ? addr.timeoutMillisecond : 1000,
+	register(callback) {
+	}
 
-							register: sample.register,
-							quantity: sample.quantity,
-							category: sample.category,
-							functioncode: sample.functioncode,
+	heartbeat() {
+		console.log(`it is alive @${__filename},`);
+	}
 
+	disconnect() {
+		return new Promise(function (resolve, reject) {
+			setTimeout(resolve, 0);
+		});
+	}
 
-							flash: sample.flash ? sample.flash : [0xef],
-						}
-						res.physicals.push(scramble)
-					}
-				}
-			}
-		}
-		console.log(res.physicals)
-		return
-	},
+	close() {
+		this.disconnect()
+		.then()
+		.catch();
+	}
 
-	orchestrate: /**/async function (req, res, next) {
-		let datasourceUnreachable = new Map()
-		let datasourceOnline = new Map()
+	connect(options = null) {
+		let that = this;
 
-		let array = bootstrap()
-		if (req) {
-			// let's assume req is array~
-			log.trace(req)
-			let temp = req.concat(array)
-			array = temp
-		}
-		// 
-		console.log(`${array.length} physical spaces loaded`)
+		return new Promise(function (resolve, reject) {
+			const socket = new net.Socket();
+			that.client = new modbus.client.TCP(socket, options.slave, options.timeoutMilli);
 
-		// query
-		for (var i = 0; i < array.length; i++) {
-			let e = array[i]
-			if (e.ip && e.port && e.subordinatorNumber && e.functioncode && e.register && e.quantity && e.timeoutMillisecond) {
-				await inventory.acquire(
-					e.ip,
-					e.port,
-					e.subordinatorNumber,
-					e.functioncode,
-					e.register,
-					e.quantity,
-					e.timeoutMillisecond,
-					e.flash)
-					.then((responses) => {
-						// raw data claim
-						log.debug(responses)
+			socket.on('connect', function () {
+				resolve(that.client);
+			});
 
-						let buffer = responses.response._body._valuesAsBuffer
-						let hexstr = buffer.toString('hex')
-						hexstr['updatedAt'] = new Date(responses.metrics.receivedAt)
-						log.debug(hexstr)
+			socket.on('error', (error) => {
+				error.ip = ip
+				error.port = port
+				socket.end()
+				reject(error)
+			});
 
-						let list = []
-						for (let i = 0; i < responses.response._body._valuesAsBuffer.length / 2; i += 2) {
-							let focused = responses.response._body._valuesAsBuffer.slice(i * 2, i * 2 + 2)
-							let win = Buffer.from(focused)
-							list.push((responses.request._body._start + i) + ':' + win.readInt16BE(0) + '(Int16),' + win.toString('hex') + '(HEX);')
-						}
-						res.write(`<p> ${list.length} :: ${e}</p>`)
-						// datasourceOnline.set(e , responses)
-						res.push(hexstr)
-						next(responses)
-					})
-					.catch((error) => {
-						// datasourceUnreachable.set(e, error)
-						log.error(error)
-					})
+			socket.connect({
+				'host': ip,                             //'192.168.2.42',
+				'port': port,                           //'502'
+			});
+
+		});
+	}
+
+	commissioning(spaces){
+		let that = this;
+		return new Promise(function (resolve, reject) {			
+			if(that.client){
+				let client = that.client;
+
+				switch (spaces.fc) {
+					case 1:
+						client.readCoils(register, count).then(function (resp) {
+							socket.end()
+							resolve(resp)
+						}).catch(function (error) {
+							error.detail = entry
+							socket.end()
+							reject(error)
+						})
+						break;
+	
+					case 2:
+						client.readDiscreteInputs(register, count).then(function (resp) {
+							socket.end()
+							resolve(resp)
+						}).catch(function (error) {
+							error.detail = entry
+							socket.end()
+							reject(error)
+						})
+						break;
+	
+					case 3:
+						client.readHoldingRegisters(register, count).then(function (resp) {
+							socket.end()
+							resolve(resp)
+						}).catch(function (error) {
+							error.detail = entry
+							socket.end()
+							reject(error)
+						});
+						break;
+	
+					case 4:
+						client.readInputRegisters(register, count).then(function (resp) {
+							socket.end()
+							resolve(resp)
+						}).catch(function (error) {
+							error.detail = entry
+							socket.end()
+							reject(error)
+						});
+						break;
+	
+					case 5:
+						client.writeSingleCoil(register, outputs).then(function (resp) {
+							socket.end()
+							resolve(resp)
+						}).catch(function (error) {
+							error.detail = entry
+							socket.end()
+							reject(error)
+						});
+						break;
+	
+					case 6:
+						client.writeSingleRegister(register, outputs).then(function (resp) {
+							socket.end()
+							resolve(resp)
+						}).catch(function (error) {
+							error.detail = entry
+							socket.end()
+							reject(error)
+						});
+						break;
+	
+					case 15:
+						client.writeMultipleCoils(register, outputs).then(function (resp) {
+							socket.end()
+							resolve(resp)
+						}).catch(function (error) {
+							error.detail = entry
+							socket.end()
+							reject(error)
+						});
+						break;
+	
+					case 16:
+						client.writeMultipleRegisters(register, outputs).then(function (resp) {
+							socket.end()
+							resolve(resp)
+						}).catch(function (error) {
+							error.detail = entry
+							socket.end()
+							reject(error)
+						});
+						break;
+	
+					default:
+						socket.end()
+						reject('Unsupported function code =' + fc + '##')
+						break;
+				}//switch()
 			} else {
-				throw new Error(`missing parameters: ${__filename}`)
+				reject(`error while acquire remote unit@ ${__filename}`);
 			}
-		}
-		return
-	},
-
+		})
+	}
 }
+util.inherits(modbustcp, events.EventEmitter)
+exports.modbustcp = modbustcp
